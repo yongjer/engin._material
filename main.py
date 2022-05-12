ca3c35e512f15c0ee348cdbd1d6b476eb80de614
yongjer <54315206+yongjer@users.noreply.github.com>
2022-05-12 17:44:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 diameter = 12.8
 init_length = 50.8
 stress = lambda x: x / (((diameter/2)*(10**(-3)))**2 * np.pi) * (10**(-6))
-strain =  lambda x: (x-init_length) / init_length
+strain = lambda x: (x-init_length) / init_length
 force = np.array([12700, 25400, 38100, 50800, 76200, 89100, 92700, 102500, 107800, 119400, 128300, 149700, 159000, 160400, 159500, 151500, 124700])
 length = np.array([50.825, 50.851, 50.876, 50.902, 50.952, 51.003, 51.054, 51.181, 51.308, 51.562, 51.816 ,52.832, 53.848, 54.356, 54.864, 55.880, 56.642])
 lines = plt.plot(strain(length), stress(force))
